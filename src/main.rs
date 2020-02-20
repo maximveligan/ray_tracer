9f53de0d9260a5786f1c4d8c8a4e35d64e9b1d56
maximveligan <maximveligan@gmail.com>
2020-02-19 21:38:39 -0800

    Ran rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-extern crate image;
 extern crate cgmath;
+extern crate image;
 
 use std::fs::File;
 use std::io::Write;
 
-use cgmath::Vector3;
-use cgmath::Point3;
 use cgmath::dot;
 use cgmath::InnerSpace;
+use cgmath::Point3;
+use cgmath::Vector3;
 
-use image::Pixel;
-use image::ImageFormat;
-use image::Rgb;
-use image::Rgba;
 use image::DynamicImage;
 use image::GenericImage;
+use image::ImageFormat;
+use image::Pixel;
+use image::Rgb;
+use image::Rgba;
 
 trait Intersectable {
     fn intersection(&self, ray: &Ray) -> Option<f64>;
@@ -55,7 +55,7 @@ enum Object {
 }
 
 impl Object {
-    fn get_color(&self) -> Rgb<f64>{
+    fn get_color(&self) -> Rgb<f64> {
         match *self {
             Object::Sphere(ref s) => s.color,
             Object::Plane(ref p) => p.color,
@@ -81,8 +81,7 @@ impl Intersectable for Sphere {
         let d_squared = center_l.dot(center_l) - (adj * adj);
         if r_squared > d_squared {
             Some(adj - (r_squared - d_squared).sqrt())
-        }
-        else {
+        } else {
             None
         }
     }
@@ -92,11 +91,29 @@ impl Intersectable for Sphere {
 }
 
 fn color_mul_const(color: &Rgb<f64>, constant: f64) -> Rgb<f64> {
-    Rgb { data: [((color.data[0] * constant)/255.0).min(255.0).max(0.0), ((color.data[1] * constant)/255.0).min(255.0).max(0.0), ((color.data[2] * constant)/255.0).min(255.0).max(0.0)] }
+    Rgb {
+        data: [
+            ((color.data[0] * constant) / 255.0).min(255.0).max(0.0),
+            ((color.data[1] * constant) / 255.0).min(255.0).max(0.0),
+            ((color.data[2] * constant) / 255.0).min(255.0).max(0.0),
+        ],
+    }
 }
 
 fn color_mul_color(color: &Rgb<f64>, color2: &Rgb<f64>) -> Rgb<f64> {
-    Rgb { data: [((color.data[0] * color2.data[0])/255.0).min(255.0).max(0.0), ((color.data[1] * color2.data[1])/255.0).min(255.0).max(0.0), ((color.data[2] * color2.data[1])/255.0).min(255.0).max(0.0)] }
+    Rgb {
+        data: [
+            ((color.data[0] * color2.data[0]) / 255.0)
+                .min(255.0)
+                .max(0.0),
+            ((color.data[1] * color2.data[1]) / 255.0)
+                .min(255.0)
+                .max(0.0),
+            ((color.data[2] * color2.data[1]) / 255.0)
+                .min(255.0)
+                .max(0.0),
+        ],
+    }
 }
 
 impl Intersectable for Object {
@@ -139,15 +156,16 @@ struct Ray {
 
 const HALF_PIXEL: f64 = 0.5;
 const BLACK: Rgba<u8> = Rgba {
-    data: [70,70,70,1],
+    data: [70, 70, 70, 1],
 };
 const ALBEDO_DEFAULT: f64 = 0.99;
 
 impl Ray {
     fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
         let aspect_ratio = scene.width as f64 / scene.height as f64;
-        let canvas_x = (((x as f64 + HALF_PIXEL) / (scene.width as f64 /2.0)) - 1.0) * aspect_ratio;
-        let canvas_y = 1.0 - ((y as f64 + HALF_PIXEL) / (scene.height as f64 /2.0));
+        let canvas_x =
+            (((x as f64 + HALF_PIXEL) / (scene.width as f64 / 2.0)) - 1.0) * aspect_ratio;
+        let canvas_y = 1.0 - ((y as f64 + HALF_PIXEL) / (scene.height as f64 / 2.0));
 
         Ray {
             origin: Point3::new(0.0, 0.0, 0.0),
@@ -162,52 +180,64 @@ impl Scene {
 
         for x in 0..self.width {
             for y in 0..self.height {
-
                 let ray = Ray::create_prime(x, y, self);
 
                 for object in self.objects.iter() {
-
                     if let Some(d) = object.intersection(&ray) {
-                        
                         match self.light {
-                            Light::DirLight(ref d_l) => { 
-                                let light_p = (object.get_albedo() / std::f64::consts::PI) * d_l.intensity * object.normal_vec(&(ray.origin + (ray.direction * d))).dot(-d_l.direction.normalize()).max(0.0);
-                                image.put_pixel(x, y, rgbf64_to_rgb8(&color_mul_const(&color_mul_color(&object.get_color(), &d_l.color), light_p)).to_rgba());
+                            Light::DirLight(ref d_l) => {
+                                let light_p = (object.get_albedo() / std::f64::consts::PI)
+                                    * d_l.intensity
+                                    * object
+                                        .normal_vec(&(ray.origin + (ray.direction * d)))
+                                        .dot(-d_l.direction.normalize())
+                                        .max(0.0);
+                                image.put_pixel(
+                                    x,
+                                    y,
+                                    rgbf64_to_rgb8(&color_mul_const(
+                                        &color_mul_color(&object.get_color(), &d_l.color),
+                                        light_p,
+                                    ))
+                                    .to_rgba(),
+                                );
                             }
                             _ => panic!("not done yet"),
                         }
-                    }
-
-                    else {
+                    } else {
                         image.put_pixel(x, y, BLACK);
                     }
-
                 }
             }
         }
 
-    image
-
+        image
     }
 }
 
 fn rgbf64_to_rgb8(color: &Rgb<f64>) -> Rgb<u8> {
-    Rgb { data: [color.data[0].round() as u8, color.data[1].round() as u8, color.data[2].round() as u8]}
+    Rgb {
+        data: [
+            color.data[0].round() as u8,
+            color.data[1].round() as u8,
+            color.data[2].round() as u8,
+        ],
+    }
 }
 
 #[test]
 #[should_panic]
 fn test_scene() {
     let scene = Scene {
-        light: Light::DirLight(DirLight { 
+        light: Light::DirLight(DirLight {
             direction: Vector3 {
-                x: -8.0,
+                x: 8.0,
                 y: -10.0,
                 z: -9.0,
             },
             intensity: 1000.0,
             color: Rgb {
-                data: [230.0, 230.0, 230.0]
+                data: [230.0, 230.0, 230.0],
             },
         }),
         width: 800,
@@ -220,14 +250,14 @@ fn test_scene() {
                 z: -7.0,
             },
             color: Rgb {
-                data: [255.0, 255.0, 0.0]
+                data: [255.0, 0.0, 0.0],
             },
-            albedo: ALBEDO_DEFAULT,       
+            albedo: ALBEDO_DEFAULT,
             radius: 2.0,
         })],
     };
     let img = scene.render();
     let mut png: File = File::create("test.png").unwrap();
-//  println!("{:?}", img.raw_pixels()[3]);
+    //  println!("{:?}", img.raw_pixels()[3]);
     img.save(&mut png, ImageFormat::JPEG);
 }
